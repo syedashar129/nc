2afa3ccbe5f9679d80f2db035c3da61d5112b124
Syed Ashar <73435085+syedashar129@users.noreply.github.com>
2026-06-14 19:39:53 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)] # array of arrays
+        pair.sort(reverse=True) #  reverse by position --> 
+        
+        stack = [] # stack of time arrivas
+
+        for p, s in pair:
+            time_arrived = (target - p) / s
+            stack.append(time_arrived)
+
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+            
+        return len(stack)
+
+
+
+# 2 arrays
+# speed and position
+# dest at target mi
+
+# car can not pass a car ahead 
+# fleet = same speed and smae position
+# both reach destination is terminal point
+
+# return # of fleets
+
+
+#  1 , 2
+#  4, 6
+# 7, 10
+# 10, 10
+
+# 4, 1, 0, 7
+# 6, 3, 1, 8
+# 8, 5, 2, 9
+# 10, 7, 3, 10
+# 10, 9, 4, 10
+# 10, 11, 5, 10
